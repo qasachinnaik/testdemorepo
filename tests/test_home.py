24a27d6746738233026ec9cb1f440d18ddc88990
Sachin Naik <sachin.naik@sjinnovation.com>
2019-10-31 11:24:35 +0530

    test demo comment

--- tests/test_home.py
+++ tests/test_home.py
@@ -10,19 +10,22 @@ from pages.home import HomePage
 
 
 def test_invalid_login(base_url_live, driver):
-    """Attempt to log in with an invalid username and password"""
+    """Open homepage and check if home is present in title"""
 
     home_page = HomePage(base_url_live, driver).open()
     assert "HOME"  in home_page.verify_home_page 
     assert  "Home" in driver.title
-   
+
+ # Below link will click on view_our_work button 
+ # it will open portfolio page in new tab
+ # Also it will keep the current tab active.  
 def test_view_our_work_button(base_url_live, driver):
 	home_page = HomePage(base_url_live, driver).open()
 	home_page.switch_tab_one(driver)
 	home_page.view_our_work()
 	home_page.switch_tab_two(driver)
 	
-
+# Below link will open the portfolio page in current tab
 def test_portfolio(base_url_live, driver):
 	home_page = HomePage(base_url_live, driver).open()
 	home_page.portfolio_page_route()
